3350793f5746a21c6f41dbfc41966c77bc33fd9b
Kubo <jakub.berthoty@gmail.com>
2019-06-04 01:15:01 +0200

    Practice.

---
+++ LINQ/Practice.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LINQ
+{
+    public static class Practice
+    {
+
+        public static string Disemvowel(string input)
+        {
+            return string.Empty;
+        }
+
+        public static string TwoToOne(string s1, string s2)
+        {
+            return string.Empty;
+        }
+
+        public static List<int> RemoveMinimum(List<int> numbers)
+        {
+            return new List<int>();
+        }
+
+        public static string CharacterError(string s)
+        {
+            return string.Empty;
+        }
+
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            return new int[] { };
+        }
+
+        public static bool IsIsogram(string str)
+        {
+            return true;
+        }
+
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            return string.Empty;
+        }
+
+        public static int FindParityOutlier(int[] integers)
+        {
+            return int.MinValue;
+        }
+
+        public static string PigLatin(string str)
+        {
+            return string.Empty;
+        }
+
+        public static string ChangeWeight(string strng)
+        {
+            return string.Empty;
+        }
+
+        public static int DontGiveMeFive(int start, int end)
+        {
+            return int.MinValue;
+        }
+
+        public static string DuplicateEncode(string word)
+        {
+            return string.Empty;
+        }
+
+        public static int OnesAndZeros(int[] binaryArray)
+        {
+            return int.MinValue;
+        }
+
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+            return new int[] { };
+        }
+    }
+}
